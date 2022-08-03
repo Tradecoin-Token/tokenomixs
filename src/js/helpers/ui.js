2618590ea29a9f88a50a0836620d6c862c5e7b6b
TRDCOIN <tradecointoken@gmail.com>
2022-08-04 03:54:47 +0530

    updated

--- src/js/helpers/ui.js
+++ src/js/helpers/ui.js
@@ -37,19 +37,15 @@ civitas.ui = {
 	window_about_section: function() {
 		var out = '<a href="#" class="do-about button">' + civitas.l('About') + '</a>' +
 			'<div class="about-game">' +
-				'<a class="github" target="_blank" href="https://github.com/sizeofcat/civitas">' +
+				'<a class="github" target="_blank" href="https://github.com/Tradecoin-Token">' +
 				'<img class="tips" title="' + civitas.l('Visit the project page on GitHub') + 
 				'" src="' + civitas.ASSETS_URL + '/images/ui/github.png" /></a>' +
-				'<p>' + civitas.l('Civitas is written by <a target="_blank" ' +
-					'href="https://sizeof.cat">sizeof(cat)</a>.') + '</p>' +
+				'<p>' + civitas.l('Tokenomixs is written by DB.</a>.') + '</p>' +
 				'<p>' + civitas.l('Big thanks to') + ':</p>' +
 				'<ul>' +
-					'<li><a target="_blank" href="https://soundcloud.com/shantifax">Shantifax</a>' +
-					' for the music (Glandula Pinealis).</li>' +
-					'<li><a target="_blank" href="https://brendaneich.com/">Brendan Eich</a> ' +
-					'for Javascript.</li>' +
-					'<li><a target="_blank" href="http://bluebyte.com">Blue Byte</a> for Anno ' +
-					'1404.</li>' +
+					'<li><a target="_blank" href="https://telegram.me/wolfofwaves">Wolf</a>.</li>' +
+					'<li><a target="_blank" href="https://telegram.me/mashcrypto">Mash</a></li>' +
+					'<li><a target="_blank" href="https://telegram.me/FaithSwapMM">FSMM</a>' +
 				'</ul>' +
 			'</div>';
 		return out;
